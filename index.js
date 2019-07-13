ec9db74fc3020a5528fb968584db450f6bac538d
Ratchadaporn Thongkow <pond7pond8@gmail.com>
2019-07-13 14:14:42 +0700

    test

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ const port = 3000
 app.get('/', (req, res) => res.send('I AM PANGPOND!'))
 app.get('/about', (req, res) => res.send('หน้า about'))
 app.get('/contact', (req, res) => res.send('หน้า conutact'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
